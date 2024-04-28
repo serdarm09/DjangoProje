f3220ec77f17fd2fd8c7400a86fd1d0ad7fe16a8
Serdar Mammadov <57108059+serdarm09@users.noreply.github.com>
2024-04-28 04:07:40 +0300

    Sessions işlemler #1

--- MuhendislikProje/urls.py
+++ MuhendislikProje/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('main.urls')),
+    path('',include('account.urls')),
     path('Admin', admin.site.urls),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import models
 
-# Create your views here.
+def login_request(request):
+    return render(request,"account/login.html")
+
+def register_request(request):
+    return render(request,"account/register.html")
+
+def logout_request(request):
+    return render(request,"logout.html")
