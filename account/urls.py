233d56a18958fad6b6b8cf84867aad6c53b301ad
Serdar Mammadov <57108059+serdarm09@users.noreply.github.com>
2024-04-28 19:49:00 +0300

    Session kısımları

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path("login",views.login_request, name="login"),
-    path("register",views.register_request, name="register"),
-    path("logout",views.logout_request, name="logout"),
+    path("login",views.login_request, name='login_1'),
+    path("register",views.register_request, name='register_1'),
+    path("logout",views.logout_request, name='logout_1'),
+    path("profile",views.profile_request, name='profile_1'),
+    path("home",views.home_views, name='home_1'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-from .models import models
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def login_request(request):
-    return render(request,"account/login.html")
+    if request.user.is_authenticated:
+        return redirect("login_1")
+    if request.method == 'POST':
+        name = request.POST.get('mail')
+        password = request.POST.get('password')
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            login(request, user)
+            # Giriş başarılı olduğunda yapılacak işlemler
+            return redirect("home_1")  # Örnek bir yönlendirme
+        else:
+            error_message = "Hatalı e-posta veya şifre"
+            return render(request, 'login_1.html', {'error_message': error_message})
+    return render(request, 'login_1.html')
+
+def home_views(request):
+    return render(request, "home_1.html")
 
 def register_request(request):
-    return render(request,"account/register.html")
+    return render(request, "register_1.html")
+
+def profile_request(request):
+    return render(request,"profile_1.html")
 
 def logout_request(request):
-    return render(request,"logout.html")
+    logout(request)
+    return redirect("login_1")  
